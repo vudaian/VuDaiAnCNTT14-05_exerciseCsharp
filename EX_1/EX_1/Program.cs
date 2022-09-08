74c12ec85dd12933ff3d5a852d4c8e7f15c3caa6
vudaian <mr.vuan24@gmail.com>
2022-09-08 21:55:48 +0700

    EX Csharp

---
+++ EX_1/EX_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace bai_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ky tu can kiem tra: ");
+            char check = (char)Console.Read();
+            if ((check >= 97 && check <= 122) || (check >= 65 && check <= 90))
+            {
+                Console.WriteLine($"{check} la chu cai");
+            }
+            else Console.WriteLine($"{check} khong la chu cai");
+        }
+    }
+}
